c8cc7cfba95097bea49d2d6fc39d1415241d3e53
Rogelio Cuevas <rcuevas@Rogelios-MacBook-Pro.local>
2017-10-14 18:29:58 -0400

    add simple experiment for multiple distributions

--- initial-experiment.R
+++ initial-experiment.R
@@ -119,63 +119,14 @@ barplot(dfMetrics$metric_value, names = dfMetrics$metric_name,
 dev.off()
 
 
-
-
-
-
-
-
-P <- 1:10/sum(1:10)
-Q <- 101:110/sum(101:110)
-hist(P)
-hist(Q)
-
-mean(P)
-mean(Q)
-sd(P)
-sd(Q)
-median(P)
-median(Q)
-# combine P and Q as matrix object
-x <- rbind(P,Q)
-distance(x, method = "jaccard")
-distance(x, method = "jensen-shannon")
-
-
-# Plotting two histograms on the samle plot
-a=rnorm(100,mean=10,sd=1)
-b=rnorm(100,mean=13,sd=1)
-
-# Side by side
-# print the two histograms side-by-side
-par(mfrow=c(1,2))
-hist(a,main="")
-hist(b,main="")
-
-# Overlapped
-hist(a,xlim=c(5,18),ylim=c(0,30),breaks=10,col=rgb(1,1,0,0.7),main="",xlab="number")
-par(new=TRUE)
-hist(b,xlim=c(5,18),ylim=c(0,30),breaks=10,col=rgb(0,1,1,0.4),main="",xlab="",ylab="")
-
-getDistMethods()
-distance(rbind(a,b),method = "jensen-shannon")
-distance(rbind(a,b),method = "euclidean")
-
-
-dist.diversity(rbind(P,Q))
-
-
-
-
-
-
-
-#combine three probabilty vectors to a probabilty matrix 
+# A simple example to compare multiple distributions
 ProbMatrix <- rbind(1:10/sum(1:10), 20:29/sum(20:29),30:39/sum(30:39))
 # compute the euclidean distance between all 
 # pairwise comparisons of probability vectors
 distance(ProbMatrix, method = "euclidean")
+distance(ProbMatrix, method = "jensen-shannon")
+
+# Distance diversity seems to work only for two distributions
+#dist.diversity(ProbMatrix)
 
 
-
-distance(rbind(P,P), method = "jensen-shannon")
