de04da444a21770e810e0bfe2d521deb2417770f
Rogelio Cuevas <rcuevas@Rogelios-MacBook-Pro.local>
2017-10-14 19:52:15 -0400

    adding permutation test

--- distributional-similarity.R
+++ distributional-similarity.R
@@ -1,6 +1,7 @@
 # script to tests similarity between distributions
 # If not installed; make sure to do so for dgof package
 #install.packages("dgof")
+#install.packages("coin")
 
 # If package installed, just invoke it
 library(dgof)
@@ -18,3 +19,15 @@ z <- rnorm(30000)
 # Do x and y come from the same distribution?
 ks.test(x,y)
 ks.test(x,z)
+
+library(coin)
+
+# permutation test
+df1<-data.frame(P,Q)
+head(df1,10)
+independence_test(P~Q,df1)
+
+
+df2<-data.frame(x,y,z)
+independence_test(x~y,df2)
+independence_test(x~z,df2)
