0ab52e5a38a9c06decf8c39c7c41596cb2de45fb
Rogelio Cuevas <rcuevas@Rogelios-MacBook-Pro.local>
2017-10-14 20:07:29 -0400

    REDME updated, dgof documentation added, minor change in script

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 This repository shows a simple way of getting a number of statistical distances using R. The code realies on the version `0.0.3` of the `philentropy` package
 
-PDF documentation of this package is added to this repo for a qucik reference
+It also contains script to perform distributional tests with KS test and permutation tests. This is achieved with version `1.2` of `dgof` package and version `1.2.1` of `coin` package.
+
+PDF documentation of these packages is added to this repo for a qucik reference
 
---
+++ distributional-similarity.R
@@ -0,0 +1,32 @@
+# script to tests similarity between distributions
+# If not installed; make sure to do so for dgof package
+#install.packages("dgof")
+#install.packages("coin")
+
+# If package installed, just invoke it
+library(dgof)
+
+# Lets create two "artificial" simple distributions
+P<-rnorm(30000,mean=507,sd=0.9)
+Q<-rnorm(30000,mean=510,sd=1.1)
+
+ks.test(P,Q)
+
+# Generate artificial data
+x <- rnorm(30000)
+y <- runif(30000)
+z <- rnorm(30000)
+# Do x and y come from the same distribution?
+ks.test(x,y)
+ks.test(x,z)
+
+# Permutation tests with coin library
+library(coin)
+
+df1<-data.frame(P,Q)
+head(df1,10)
+independence_test(P~Q,df1)
+
+df2<-data.frame(x,y,z)
+independence_test(x~y,df2)
+independence_test(x~z,df2)
